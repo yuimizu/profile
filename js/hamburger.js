dfaa64b601784b08026cca4d60033d35ddbf9d1b
yuimizu <soregakoidato35@gmail.com>
2024-01-09 16:42:19 +0900

    更新

---
+++ js/hamburger.js
@@ -0,0 +1,9 @@
+$(".openbtn").click(function () {
+	$(this).toggleClass('active');
+    $("#g-nav").toggleClass('panelactive');
+});
+
+$("#g-nav a").click(function () {
+    $(".openbtn").removeClass('active');
+    $("#g-nav").removeClass('panelactive');
+});
---
+++ js/transition.js
@@ -0,0 +1,18 @@
+$(window).on('load',function(){
+    $("#splash-logo").delay(1200).fadeOut('slow');
+    
+    //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+    $("#splash").delay(1500).fadeOut('slow',function(){
+    
+    $('body').addClass('appear');
+    
+    });
+    //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+    
+    //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
+    $('.splashbg').on('animationend', function() { 
+    //この中に動かしたいJSを記載
+    });
+    //=====ここまで背景が伸びた後に動かしたいJSをまとめる
+    
+    });
